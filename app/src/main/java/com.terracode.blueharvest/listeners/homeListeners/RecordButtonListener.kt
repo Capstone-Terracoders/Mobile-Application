181b6102abf32d83efb0d99f63221b6174a7fc8f
Kenzie Young <mackenzie.k.young@maine.edu>
2024-03-17 23:55:56 -0400

    Kenzie/SUB-MAF-14: Got rid of un-needed toasts.

--- app/src/main/java/com.terracode.blueharvest/listeners/homeListeners/RecordButtonListener.kt
+++ app/src/main/java/com.terracode.blueharvest/listeners/homeListeners/RecordButtonListener.kt
@@ -2,7 +2,6 @@ package com.terracode.blueharvest.listeners.homeListeners
 
 import android.view.View
 import android.view.View.OnClickListener
-import android.widget.Toast
 import com.terracode.blueharvest.HomeActivity
 import com.terracode.blueharvest.utils.NotificationTypes
 import com.terracode.blueharvest.utils.PreferenceManager
@@ -38,11 +37,9 @@ class RecordButtonListener(private val activity: HomeActivity) : OnClickListener
         if (newStatus) {
             // If the status is true, record and send notification
             PreferenceManager.setNotification(startRecordingNotification)
-            Toast.makeText(activity, "Recording Started", Toast.LENGTH_SHORT).show()
         } else {
             // If the status is false, stop recording and send notification
             PreferenceManager.setNotification(stopRecordingNotification)
-            Toast.makeText(activity, "Recording Stopped", Toast.LENGTH_SHORT).show()
         }
     }
 }
